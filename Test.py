1e8f3ab19baae220acb07e6e9268f558794f4eb9
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-04-26 16:13:53 +0300

    added test model

--- Test.py
+++ Test.py
@@ -16,9 +16,9 @@ model.c = Param(model.I, model.J, initialize={
     (1, 1): 0, (2, 2): 0, (3, 3): 0  # initialize diagonal elements to zero
 })  # transmission cost from i to j
 model.p_max_plant = Param(model.I, model.Plants, initialize={
-    (1, 'Plant1'): 0, (1, 'Plant2'): 0, (1, 'Plant3'): 0,
-    (2, 'Plant1'): 300, (2, 'Plant2'): 0, (2, 'Plant3'): 0,
-    (3, 'Plant1'): 800, (3, 'Plant2'): 0, (3, 'Plant3'): 0
+    (1, 'Plant1'): 300, (1, 'Plant2'): 300, (1, 'Plant3'): 0,
+    (2, 'Plant1'): 300, (2, 'Plant2'): 300, (2, 'Plant3'): 0,
+    (3, 'Plant1'): 800, (3, 'Plant2'): 300, (3, 'Plant3'): 300
 })
 
 model.CHP_Plants = Set(within=model.I * model.Plants, initialize={
@@ -38,7 +38,7 @@ Cp=4.18
 massflow = 2.4
 
 model.u = Param(model.I, model.J, initialize={(1, 2): M, (1, 3): M, (2, 1): M, (2, 3): M, (3, 1): M, (3, 2): M, (1, 1): 0, (2, 2): 0, (3, 3): 0})  # transmission capacity limit from i to j
-model.d = Param(model.I, initialize={1: 400, 2: 0, 3: 400})  # net supply (supply - demand) in node i
+model.d = Param(model.I, initialize={1: 1500, 2: 0, 3: 400})  # net supply (supply - demand) in node i
 # model.p_max = Param(model.I, initialize={1: 0, 2: 2000, 3: 0})  # maximum production capacity at node i
 # model.c_gen = Param(model.I, initialize={1: 30, 2: 10, 3: 30}) # generation cost at node i
 model.c_gen = Param(model.I, model.Plants, initialize={
