891cbaf50ea42e4963026c76d7c1a49facf0990b
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-04-26 20:00:00 +0300

    added test model

--- Test.py
+++ Test.py
@@ -208,3 +208,9 @@ for t in model.T:
         for j in model.J:
             if model.Ql[i,j,t].value > 0:
                 print(f"Loss from node {j} to node {i}: {model.Ql[i,j,t].value:.2f}")
+
+print("z:")
+for t in model.T:
+    for i in model.I:
+        for j in model.J:
+                print(model.z[i,j,t].value)
