226b978231a55593abb83bbc21fcc81d111a7061
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-03 12:33:10 +0300

    added scripts gas

--- HeatNet.py
+++ HeatNet.py
@@ -211,8 +211,7 @@ def heatloss_bin2(model, i,j,t):
 model.heatloss_bin2 = Constraint(model.I, model.J, model.T, rule=heatloss_bin2)
 #Add Fairness constraint 
 solver = SolverFactory("knitro");
-
-results = solver.solve(model, tee=True)
+results = solver.solve(model, options={'mip_outlevel' : 2}, tee=True)
 
 # solver = SolverFactory("mindtpy")
 # results = solver.solve(model,mip_solver="gurobi",nlp_solver="ipopt",tee=True)
