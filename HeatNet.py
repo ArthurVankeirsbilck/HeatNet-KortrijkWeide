a679e40d8bc50e34dfa367219c2b3c93268da468
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-04 09:45:43 +0300

    octeract

--- HeatNet.py
+++ HeatNet.py
@@ -104,9 +104,9 @@ hour = 60
 model.p_max_plant = Param(model.I, model.Plants, initialize={
     (1, 'Plant1'): 0.751*hour, (1, 'Plant2'):0, (1, 'Plant3'):0,
     (2, 'Plant1'): 2.312*hour,  (2, 'Plant2'):0.045*hour, (2, 'Plant3'):0.34*hour,
-    (3, 'Plant1'): 2.312*hour, (3, 'Plant2'):0,(3, 'Plant3'):0,
+    (3, 'Plant1'): 0, (3, 'Plant2'):0,(3, 'Plant3'):0,
     (4, 'Plant1'): 0.350*hour, (4, 'Plant2'):0, (4, 'Plant3'): 0,
-    (5, 'Plant1'): 2.312*hour, (5, 'Plant2'): 2.312*hour, (5, 'Plant3'): 0,
+    (5, 'Plant1'): 0, (5, 'Plant2'): 0, (5, 'Plant3'): 0,
     (6, 'Plant1'): 0.160*hour, (6, 'Plant2'): 0, (6, 'Plant3'): 0,
     (7, 'Plant1'): 0, (7, 'Plant2'): 0, (7, 'Plant3'): 0
 })
