267361f41976150f9c07040b11a2c24620631689
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-08 10:16:28 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -40,7 +40,7 @@ def CHP_feasible_area(yA):
 
     return xA, xB, yB, xC, yC, xD, yD
 
-hours=100
+hours=336
 node1_demands = df["KWEA_dec_jan"].iloc[0:hours].to_list()
 node2_demands = [0]*hours
 node3_demands = [300]*hours
@@ -103,12 +103,12 @@ model.c = Param(model.I, model.J, initialize=
 (7, 1): 50, (7, 2): 50, (7, 3): 50, (7, 4): 50, (7, 5): 50, (7, 6): 50, (7, 7): 0}
 )  # transmission cost from i to j
 model.p_max_plant = Param(model.I, model.Plants, initialize={
-    (1, 'Plant1'): 751*3600, (1, 'Plant2'):0, (1, 'Plant3'):0,
-    (2, 'Plant1'): 2312*3600,  (2, 'Plant2'):45*3600, (2, 'Plant3'):340*3600,
+    (1, 'Plant1'): 751, (1, 'Plant2'):0, (1, 'Plant3'):0,
+    (2, 'Plant1'): 2312,  (2, 'Plant2'):45, (2, 'Plant3'):340,
     (3, 'Plant1'): 0, (3, 'Plant2'):0,(3, 'Plant3'):0,
-    (4, 'Plant1'): 350*3600, (4, 'Plant2'): 0, (4, 'Plant3'): 0,
+    (4, 'Plant1'): 350, (4, 'Plant2'): 0, (4, 'Plant3'): 0,
     (5, 'Plant1'): 0, (5, 'Plant2'): 0, (5, 'Plant3'): 0,
-    (6, 'Plant1'): 160*3600, (6, 'Plant2'): 0, (6, 'Plant3'): 0,
+    (6, 'Plant1'): 160, (6, 'Plant2'): 0, (6, 'Plant3'): 0,
     (7, 'Plant1'): 0, (7, 'Plant2'): 0, (7, 'Plant3'): 0
 })
 CHP_plants ={
