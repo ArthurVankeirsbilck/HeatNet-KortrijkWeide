262cc933c047defcc3da73370d529c768d7d1328
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-08 16:14:18 +0300

    model

--- HeatNet.py
+++ HeatNet.py
@@ -272,17 +272,17 @@ def ramping_1(model, i,p,t):
     if t == 0:
         return Constraint.Skip 
     else:
-        return 1*model.p_max_plant[i,p] <= model.p[p,i,t] - model.p[p,i,t-1]
+        return 0.04*model.p_max_plant[i,p] <= model.p[p,i,t] - model.p[p,i,t-1] <= 0.04*model.p_max_plant[i,p]
 
 model.ramping_1 = Constraint(model.I, model.Plants, model.T, rule=ramping_1)
 
-def ramping_2(model, i,p,t):
-    if t == 0: 
-        return Constraint.Skip 
-    else:
-        return model.p[p,i,t] - model.p[p,i,t-1] <= 1*model.p_max_plant[i,p]
+# def ramping_2(model, i,p,t):
+#     if t == 0: 
+#         return Constraint.Skip 
+#     else:
+#         return model.p[p,i,t] - model.p[p,i,t-1] <= 0.04*model.p_max_plant[i,p]
 
-model.ramping_2 = Constraint(model.I, model.Plants, model.T, rule=ramping_2)
+# model.ramping_2 = Constraint(model.I, model.Plants, model.T, rule=ramping_2)
 
 # def ramping_3(model, i,p,t):
 #     if t == model.T.first(): 
