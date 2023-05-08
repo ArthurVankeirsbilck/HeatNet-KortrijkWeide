d859d6e5234e482ee46721ff06ab2bb46c981024
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-08 15:34:46 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -105,9 +105,9 @@ model.c = Param(model.I, model.J, initialize=
 model.p_max_plant = Param(model.I, model.Plants, initialize={
     (1, 'Plant1'): 751, (1, 'Plant2'):0, (1, 'Plant3'):0,
     (2, 'Plant1'): 2312,  (2, 'Plant2'):45, (2, 'Plant3'):340,
-    (3, 'Plant1'): 0, (3, 'Plant2'):0,(3, 'Plant3'):0,
+    (3, 'Plant1'): 500, (3, 'Plant2'):500,(3, 'Plant3'):500,
     (4, 'Plant1'): 350, (4, 'Plant2'): 0, (4, 'Plant3'): 0,
-    (5, 'Plant1'): 0, (5, 'Plant2'): 0, (5, 'Plant3'): 0,
+    (5, 'Plant1'): 0, (5, 'Plant2'): 0, (5, 'Plant3'): 500,
     (6, 'Plant1'): 160, (6, 'Plant2'): 0, (6, 'Plant3'): 0,
     (7, 'Plant1'): 0, (7, 'Plant2'): 0, (7, 'Plant3'): 0
 })
