5cda3c4cd65531dbb7d8624aed2a5aa3d328008f
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-09 20:24:26 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -375,8 +375,8 @@ for t in model.T:
             if i == j:
                 pass
             else:
-                if model.Ppump[i,j,t].value > 0:
-                    print("power {} -> {}: {}".format(j,i, model.v[i,j,t].value))
+                if model.massflow[i,j,t] > 0:
+                    print("power {} -> {}: {}".format(j,i, model.massflow[i,j,t]))
 
 # print("Heatloss cost:")
 # for t in model.T:
