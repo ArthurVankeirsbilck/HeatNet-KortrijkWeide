83ff7c0deeac4e3c75b430e53276794f2031e9be
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-09 20:05:30 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -293,7 +293,25 @@ def ramping_2(model, i,p,t):
     else:
         return model.p[p,i,t] - model.p[p,i,t-1] >= model.ramp_rate[i,p]*model.p_max_plant[i,p]
 
-# model.ramping_2 = Constraint(model.I, model.Plants, model.T, rule=ramping_2)
+model.ramping_2 = Constraint(model.I, model.Plants, model.T, rule=ramping_2)
+
+
+# def flow_speed(model, i,j,t):
+#     return model.v[i,j,t] == model.massflow[i,j,t]/(971.79*(3.14*(model.Di[i,j]/2)^2))
+
+# def reynolds(model,i,j,t):
+#     return model.Re[i,j,t] == (971.79*model.v[i,j,t]*model.Di[i,j])/0.000355
+
+# def pressure_drop(model,i,j,t):
+#     return model.Dp[i,j,t] == (model.L[i,j]/model.Di[i,j])*model.f[i,j,t]*971.79*((model.v[i,j,t]^2)/2)
+
+# def networkloss(model,i,j,t):
+#     return NWloss[i,j,t] == 2*(model.Dp[i,j,t]+60000)
+
+# def Pumppower(model, i,j,t):
+#     return Ppump == ((model.Dp[i,j,t]/model.massflow[i,j,t])*NWloss[i,j,t])/0.7
+
+
 
 # def ramping_3(model, i,p,t):
 #     if t == model.T.first(): 
