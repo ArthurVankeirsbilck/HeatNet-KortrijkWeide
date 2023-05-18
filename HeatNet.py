4997c0fdb83f1996ee738f2e4e44c89e43bb1285
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-18 10:39:25 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -234,12 +234,12 @@ model.obj = Objective(
     expr=sum(model.c[i,j]*model.x[i,j,t] + model.Ppump[i,j,t]*model.P_elec[t] for j in model.J for i in model.I for t in model.T) + sum(model.c_gen[i,p,t]*model.p[p,i,t] - model.P_elec[t]*model.P_el[p,i,t] for p in model.Plants for i in model.I for t in model.T), sense=minimize)
 
 def balance_constraint_rule(model, i,j,t):
-    return sum(model.x[i, j, t] - model.x[j, i, t] for j in model.J) + sum(model.p[p,i,t] for p in model.Plants) == model.d[i,t] + model.Ql[i,j,t]
+    return sum(model.x[i, j, t] - model.x[j, i, t] for j in model.J) + sum(model.p[p,i,t] for p in model.Plants) == model.d[i,t]
 
 model.balance_constraint = Constraint(model.I, model.J, model.T , rule=balance_constraint_rule)
 
 def heat_flow_constraint(model, i, j,t):
-    return Cp*model.massflow[i,j,t]*(model.Ts[i,j,t]-model.Tr[i,j,t]) == model.d[i,t] + model.Ql[i,j,t]
+    return Cp*model.massflow[i,j,t]*(model.Ts[i,j,t]-model.Tr[i,j,t]) == model.d[i,t]
 
 model.heat_flow_constraint = Constraint(model.I, model.J, model.T, rule=heat_flow_constraint)
 
@@ -249,7 +249,7 @@ def capacity_constraint_rule(model, i, j,t):
 model.capacity_constraint = Constraint(model.I, model.J, model.T, rule=capacity_constraint_rule)
 
 def capacity_constraint_rule_2(model, i, j, t):
-    return sum(model.x[i, j,t] for i in model.I for j in model.J) <= 5000
+    return sum(model.x[i, j,t] for i in model.I for j in model.J) <= 900
 
 model.capacity_constraint_rule_2 = Constraint(model.nodes_connected_to_pipe1, model.nodes_connected_to_pipe1, model.T, rule=capacity_constraint_rule_2)
 
