a263b8a68f87dd691803b080c91ce0f6071457e1
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-18 10:32:10 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -231,7 +231,7 @@ epsilon = 0.00001
 Cramping = 0.1
 # objective
 model.obj = Objective(
-    expr=sum(model.c[i,j]*model.x[i,j,t] + model.Ppump[i,j,t]*model.P_elec[t] + model.Ql[i,j,t]*model.c_gen[i,p,t] for j in model.J for i in model.I for t in model.T) + sum(model.c_gen[i,p,t]*model.p[p,i,t] - model.P_elec[t]*model.P_el[p,i,t] for p in model.Plants for i in model.I for t in model.T), sense=minimize)
+    expr=sum(model.c[i,j]*model.x[i,j,t] + model.Ppump[i,j,t]*model.P_elec[t] + model.Ql[i,j,t] for j in model.J for i in model.I for t in model.T) + sum(model.c_gen[i,p,t]*model.p[p,i,t] - model.P_elec[t]*model.P_el[p,i,t] for p in model.Plants for i in model.I for t in model.T), sense=minimize)
 
 def balance_constraint_rule(model, i,j,t):
     return sum(model.x[i, j, t] - model.x[j, i, t] for j in model.J) + sum(model.p[p,i,t] for p in model.Plants) == model.d[i,t]
