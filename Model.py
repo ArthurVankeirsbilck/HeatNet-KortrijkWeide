053039caa8f54eb73d49f34a31aea78f978e6e2c
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-20 12:22:58 +0300

    test

--- Model.py
+++ Model.py
@@ -71,6 +71,7 @@ model.M_flow = Var(model.PowerLines, model.T, bounds=(0, 40),within=NonNegativeR
 model.T_supply = Var(model.PowerLines, model.T, bounds=(60, 120),within=NonNegativeReals)
 model.T_return = Var(model.PowerLines, model.T, bounds=(30, 120),within=NonNegativeReals)
 model.T_mixed = Var(model.PowerLines, model.T, bounds=(30, 120), within=NonNegativeReals)
+model.T_incoming = Var(model.N, model.T, bounds=(30, 120), within=NonNegativeReals)
 model.X = Var(model.N, model.T, within=Binary)
 model.Z = Var(model.N, model.N, within=Binary)
 
