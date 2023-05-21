95a0603a917d48593be53dddef89b46688fc3c3d
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-21 11:09:05 +0300

    test

--- Model.py
+++ Model.py
@@ -124,7 +124,7 @@ def export_constraint_rule(model, i, t):
 model.export_constraint = Constraint(model.N, model.T, rule=export_constraint_rule)
 
 def transmission_constraint_rule(model, t):
-    return sum(model.P[i, t] - model.E[i, t] + model.I[i, t] for i in model.N) <= model.P_transmission
+    return sum(model.P[p, i, t]  - model.E[i, t] + model.I[i, t] for i in model.N for p in model.Plants) <= model.P_transmission
 model.transmission_constraint = Constraint(model.T, rule=transmission_constraint_rule)
 
 # Constraint
@@ -141,7 +141,7 @@ def consistency_constraint_rule(model, i, t):
 model.consistency_constraint = Constraint(model.N, model.T, rule=consistency_constraint_rule)
 
 def energy_balance_constraint_rule(model, pipe, t):
-    return sum(model.P[i, t] * model.X[i, t] for i in model.N) == model.M_flow[pipe, t] * (0.0007*model.T_supply[pipe, t]+4.1484) * (model.T_supply[pipe, t] - model.T_return[pipe, t])
+    return sum(model.P[p, i, t] * model.X[i, t] for i in model.N for p in model.Plants) == model.M_flow[pipe, t] * (0.0007*model.T_supply[pipe, t]+4.1484) * (model.T_supply[pipe, t] - model.T_return[pipe, t])
 
 model.energy_balance_constraint = Constraint(model.PowerLines, model.T, rule=energy_balance_constraint_rule)
 
