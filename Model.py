863a3fa36fe57cb4423e0fd0392fd4b531febe67
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-22 21:10:19 +0300

    test

--- Model.py
+++ Model.py
@@ -236,7 +236,7 @@ for t in model.T:
 
 for i in model.N:
     for p in model.Plants:
-        print("plant {}, node {}: {}{}{}".format(p,i,model.P[p, i, 1].value, model.E[i, 1].value, model.I[i, 1].value))
+        print("plant {}, node {}: {}, {}, {}".format(p,i,model.P[p, i, 1].value, model.E[i, 1].value, model.I[i, 1].value))
 
 print(model.Ql[1,2,1].value)
 print(model.Ql[2,3,1].value)
