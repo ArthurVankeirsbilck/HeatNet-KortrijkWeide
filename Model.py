ae0cbb082f097ec42ac7a9a327133af28d9f40f4
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-22 09:41:15 +0300

    test

--- Model.py
+++ Model.py
@@ -221,7 +221,7 @@ def heatloss_constraint(model, i, t,pipe):
     if i == 4:
         return Constraint.Skip
     else:
-        return model.Ql[i,i+1,t] == ((((2.0*3.14*0.05*50*(model.T_supply[pipe, t]-model.T_return[pipe, t]))/math.log(0.125/0.1022))/1000))
+        return model.Ql[i,i+1,t] == ((((2.0*3.14*0.05*50*(model.T_supply[pipe, t]-model.T_return[pipe, t]))/math.log(0.125/0.1022))/1000))* model.X[i, t]
 model.heatloss_constraint = Constraint(model.N, model.T, model.PowerLines, rule=heatloss_constraint)
 
 solver = SolverFactory("octeract");
