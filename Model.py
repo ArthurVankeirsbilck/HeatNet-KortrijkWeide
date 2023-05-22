a857ade62ad3b3b3ace8d0caa0217f4b9ef26d18
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-22 09:36:54 +0300

    test

--- Model.py
+++ Model.py
@@ -229,5 +229,5 @@ results = solver.solve(model,tee=True)
 
 for t in model.T:
     for pipe in model.PowerLines:
-        print(pipe, t, model.M_flow[pipe, t].value,model.T_supply[pipe, t].value, model.T_return[pipe, t].value)
+        print(pipe, t, model.M_flow[pipe, t].value,model.T_supply[pipe, t].value, model.T_return[pipe, t].value, model.Ql[i,i+1,t].value)
         
