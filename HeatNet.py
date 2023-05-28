2d75c0dbc93a338928a2fcb95310b15938c0c790
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-05-28 17:05:14 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -26,25 +26,19 @@ model.diameter = Param(initialize=0.125)
 model.I = Var(model.N, model.T, within=NonNegativeReals)
 model.E = Var(model.N, model.T, within=NonNegativeReals)
 model.m_pipe = Var(model.N, model.T, within=NonNegativeReals)
-model.v = Var(model.N, model.T, within=NonNegativeReals)
-model.dp= Var(model.N, model.T, within=NonNegativeReals)
+model.Ppump= Var(model.N, model.T, within=NonNegativeReals)
 model.m_N_ex = Var(model.N, model.T, within=NonNegativeReals, bounds=(0, 30))
 model.m_N_im = Var(model.N, model.T, within=NonNegativeReals, bounds=(0, 30))
 model.Z1 = Var(model.N, model.T, domain=Binary)
 model.Z2 = Var(model.N, model.T, domain=Binary)
 model.p = Var(model.N, model.T, within=NonNegativeReals)
 
-model.obj = Objective(expr=sum(model.p[i,t]*model.Cgen[i] for i in model.N for t in model.T), sense=minimize)
+model.obj = Objective(expr=sum(model.p[i,t]*model.Cgen[i]  + model.Ppump[i,t] for i in model.N for t in model.T), sense=minimize)
 
-def speed(model, i ,t):
-    return model.v[i,t] == model.m_pipe[i,t]/model.rho/model.area 
+def pumppower(model, i ,t):
+    return model.Ppump[i,t] == 186.42*model.m_pipe[i,t] - 530.08
 
-model.speed = Constraint(model.N, model.T, rule=speed) 
-
-def pressureloss(model, i ,t):
-    return model.dp[i,t] == (50/model.diameter) * 0.01715437 * model.rho * (model.v[i,t]**2/2)
-
-model.pressureloss = Constraint(model.N, model.T, rule=pressureloss) 
+model.pumppower = Constraint(model.N, model.T, rule=pumppower) 
 
 def demandcons(model, i, t):
     return  model.I[i,t]*model.Z1[i,t] - model.E[i,t]*model.Z2[i,t] + model.p[i,t] == model.demand[i,t]
@@ -85,12 +79,12 @@ def productionconstraint(model,i,t):
 
 model.productionconstraint = Constraint(model.N,model.T, rule=productionconstraint)
 
-solver = SolverFactory("octeract");
+solver = SolverFactory("gurobi");
 results = solver.solve(model,tee=True)
 
 for i in model.N:
     print("pipeflow at {}: {}".format(i,model.m_pipe[i,1].value))
-    print("pipespeed at {}: {}".format(i,model.v[i,1].value))
+    # print("Pumppower at {}: {}".format(i,model.Ppump[i,1].value))
     print("production at {}: {}".format(i, model.p[i,1].value))
     print("massflow import at {}: {}".format(i,model.m_N_im[i,1].value))
     print("massflow export at {}: {}".format(i,model.m_N_ex[i,1].value))
