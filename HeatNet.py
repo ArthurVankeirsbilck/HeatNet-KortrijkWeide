dce5faa5bb9d84ea76fd7f9be3a562f85dd1a524
Arthur Vankeirsbilck <arthur@trikthom.com>
2023-06-17 10:39:37 +0300

    test

--- HeatNet.py
+++ HeatNet.py
@@ -303,7 +303,7 @@ print("Model type:", model_type)
 
 print("Model reading done, solving begins...")
 
-solver = SolverFactory("Cplex");
+solver = SolverFactory("cplex");
 results = solver.solve(model,tee=True)
 
 print("Model solved, writing results...")
